306f32da176b35323dbcbec0349d604e0b027bc8
Emilio Barrera <barrera_emilio@yahoo.es>
2025-04-03 15:39:07 +0100

    Done

--- TheArchwayLeisureCentreSystem/src/Presentation/LoginPage.java
+++ TheArchwayLeisureCentreSystem/src/Presentation/LoginPage.java
@@ -302,6 +302,8 @@ public class LoginPage extends javax.swing.JFrame
         }
         //</editor-fold>
 
+        
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable()
         {
